ec8f453e2e71a8576c41e8adc36b13548c2f6ef8
hassansalah <hs8266@gmail.com>
2025-02-19 19:49:49 +0200

    -

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -300,27 +300,5 @@ class UserController extends Controller
         ];
     }
 
-    /**
-     * Return a success response.
-     */
-    private function successResponse($message, $data = null)
-    {
-        return response()->json([
-            'status' => true,
-            'message' => $message,
-            'data' => $data,
-        ]);
-    }
 
-    /**
-     * Return an error response.
-     */
-    private function errorResponse($message, $statusCode)
-    {
-        return response()->json([
-            'status' => false,
-            'message' => $message,
-            'data' => null,
-        ], $statusCode);
-    }
 }
