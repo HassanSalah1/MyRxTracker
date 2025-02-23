4655e23e3334a1bb63502f2df650b49ee35d8b6c
hassansalah <hs8266@gmail.com>
2025-02-23 22:36:47 +0200

    - hassan

--- database/migrations/2025_02_23_123155_add_columns_to_users_table.php
+++ database/migrations/2025_02_23_123155_add_columns_to_users_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('fcm_token')->nullable();
-            $table->tinyInteger('notification_permission')->nullable();
+            $table->string('fcm_token')->after('email')->nullable();
+            $table->tinyInteger('notification_permission')->after('email')->nullable();
         });
     }
 
