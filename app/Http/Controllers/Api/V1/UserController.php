9b85de6249e64af8da005ecd72553bdf157719ad
hassansalah <hs8266@gmail.com>
2025-02-23 22:21:28 +0200

    -

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -310,6 +310,7 @@ class UserController extends Controller
             'photo' => $image_url,
             'request_starter_pack' => (bool) $user?->starterPacks,
             'took_starter_pack' =>  (bool)$user?->starterPacks?->where('verification_status', PacksStatus::APPROVED)->count(),
+            'fcm_token' => $user->fcm_token,
             'access_token' => $token,
         ];
     }
