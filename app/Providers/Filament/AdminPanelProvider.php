dba98cb85848aeaa196ca14c58c0e392be29bbb8
hassansalah <hs8266@gmail.com>
2025-02-23 17:38:37 +0200

    -

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,8 +27,10 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->viteTheme('resources/css/filament/theme.css')
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => "#553782",
+                'secondary' => "#CC833D",
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
