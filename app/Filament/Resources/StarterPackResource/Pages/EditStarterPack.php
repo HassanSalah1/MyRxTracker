33cd79a642b549326ef45928086e1354f39ec455
hassansalah <hs8266@gmail.com>
2025-02-24 22:08:57 +0200

    - notification

--- app/Filament/Resources/StarterPackResource/Pages/EditStarterPack.php
+++ app/Filament/Resources/StarterPackResource/Pages/EditStarterPack.php
@@ -25,26 +25,26 @@ class EditStarterPack extends EditRecord
     {
         $record = $this->record;
         if ($record->verification_status == PacksStatus::APPROVED->value) {
-            //$firebaseService = new FirebaseService();
-            // Fetch the user
-//            $user = $record->user;
-//            if ($user && $user->fcm_token) {
-//                // Send Firebase Notification
-//                $firebaseService->sendNotification(
-//                    'Verification Approved',
-//                    'Your verification status has been updated to OK!',
-//                    $user->fcm_token,
-//                    ['pack_id' => $record->id]
-//                );
-//            }
+            $firebaseService = new FirebaseService();
+             //Fetch the user
             $user = $record->user;
-            if ($user) {
-                $user->notify(new VerificationApprovedNotification(
+            if ($user && $user->fcm_token) {
+                // Send Firebase Notification
+                $firebaseService->sendNotification(
                     'Verification Approved',
                     'Your verification status has been updated to OK!',
-                    ['type' => NotificationStatus::HOME->value]
-                ));
-                }
+                    $user->fcm_token,
+                    ['pack_id' => $record->id]
+                );
+            }
+//            $user = $record->user;
+//            if ($user) {
+//                $user->notify(new VerificationApprovedNotification(
+//                    'Verification Approved',
+//                    'Your verification status has been updated to OK!',
+//                    ['type' => NotificationStatus::HOME->value]
+//                ));
+//                }
         }
     }
 }
